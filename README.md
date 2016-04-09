43c1c833088ea6a1354c1ddc2dbc7aa5a2af49d6
arai <arai_a@mac.com>
2016-04-09 09:00:08 +0900

    Merge extract.py to build.py

--- README.md
+++ README.md
@@ -16,13 +16,10 @@ $ python build.py init
 
 ```
 $ python build.py update
-$ python extract.py
 ```
 
-`update` command fetches changesets from https://github.com/tc39/ecma262/ , and  generates HTML file for each revisions.
-HTML files are stored as `./history/{hash}.html`, and it's cached for 2nd invocation.
-
-`extract.py` extracts data from generated HTML file, and save the data into `./history/{hash}.json`.
+`update` command fetches changesets from https://github.com/tc39/ecma262/ , and  generates HTML file and JSON file for each revisions.
+HTML files are stored as `./history/{hash}.html`, and JSON files are stored as `./history/{hash}.json`, and they're cached for 2nd invocation.
 
 ## Update PRs
 
@@ -31,5 +28,5 @@ $ python build.py pr PR_NUMBER
 $ python extract.py
 ```
 
-`pr` command retrieves information about specified PR and generated HTML file for each revisions in the PR.
+`pr` command retrieves information about specified PR and generated HTML file and JSON file for each revisions in the PR.
 It will fail if the PR is not mergeable.
--- build.py
+++ build.py
@@ -6,6 +6,8 @@ from __future__ import print_function
 
 import glob
 import json
+import lxml.html
+import lxml.etree
 import os
 import shutil
 import subprocess
@@ -70,6 +72,7 @@ def update_master():
     for line in p.stdout:
         hash = line.strip()
         generate_html(hash, False, '')
+        generate_json(hash, '')
     p.wait()
 
 def update_revs():
@@ -147,6 +150,7 @@ def get_pr(pr):
     for commit in data:
         hash = commit['sha']
         generate_html(hash, True, 'PR/{}/'.format(pr))
+        generate_json(hash, 'PR/{}/'.format(pr))
         revs.append(hash)
     revs.reverse()
 
@@ -165,6 +169,110 @@ def get_all_pr():
     for pr in data:
         print(pr['number'])
         get_pr(pr['number'])
+
+def get_text(node):
+    return ''.join([x for x in node.itertext()])
+
+def check_id(node):
+    if 'id' not in node.attrib:
+        return False
+    if not node.attrib['id'].startswith('sec-'):
+        return False
+    return True
+
+def get_sec_nodes(dom):
+    return filter(check_id, dom.xpath('//emu-clause') + dom.xpath('//emu-annex'))
+
+def get_sec_list(dom):
+    sec_list = []
+    sec_num_map = dict()
+    sec_title_map = dict()
+
+    for node in get_sec_nodes(dom):
+        id = node.attrib['id']
+
+        h1 = node.xpath('./h1')[0]
+
+        secnum = h1.xpath('./span[@class="secnum"]')[0]
+        num = get_text(secnum)
+        #secnum.drop_tree()
+
+        utils = h1.xpath('./span[@class="utils"]')[0]
+        utils.drop_tree()
+
+        title = get_text(h1).replace(num, '')
+
+        sec_list.append(id)
+        sec_num_map[id] = num
+        sec_title_map[id] = title
+
+    return sec_list, sec_num_map, sec_title_map
+
+def exclude_subsections(dom, sec_list, sec_num_map, sec_title_map):
+    for node in get_sec_nodes(dom):
+        id = node.attrib['id']
+
+        num = sec_num_map[id]
+        title = sec_title_map[id]
+
+        h1 = lxml.html.Element('h1')
+        secnum = lxml.html.Element('span', {'class': 'secnum'})
+        secnum.text = num
+        h1.append(secnum)
+        secnum.tail = title
+        h1.tail = '...'
+
+        box = lxml.html.Element('div')
+        box.append(h1)
+
+        node.getparent().replace(node, box)
+
+        dom.body.append(node)
+
+def extract_sec_html(dom, sec_list, sec_num_map, sec_title_map):
+    sec_data = dict()
+
+    for node in get_sec_nodes(dom):
+        id = node.attrib['id']
+        num = sec_num_map[id]
+        title = sec_title_map[id]
+        html = lxml.etree.tostring(node, method='html')
+
+        entry = dict()
+        entry['num'] = num
+        entry['title'] = title
+        entry['html'] = html
+        sec_data[id] = entry
+
+    data = dict()
+    data['secList'] = sec_list
+    data['secData'] = sec_data
+
+    return data
+
+def extract_sections(filename):
+    in_filename = '{}.html'.format(filename)
+    out_filename = '{}.json'.format(filename)
+
+    if os.path.exists(out_filename):
+        print('skip {}'.format(out_filename))
+        return
+
+    print(out_filename)
+
+    with open(in_filename, 'r') as in_file:
+        dom = lxml.html.fromstring(in_file.read())
+
+        sec_list, sec_num_map, sec_title_map = get_sec_list(dom)
+        exclude_subsections(dom, sec_list, sec_num_map, sec_title_map)
+        data = extract_sec_html(dom, sec_list, sec_num_map, sec_title_map)
+
+        with open(out_filename, 'w') as out_file:
+            out_file.write(json.dumps(data))
+
+def generate_json(hash, subdir):
+    basedir = './history/{}'.format(subdir)
+    extract_sections('{}{}'.format(basedir, hash))
 
 def usage():
     print('Usage:')
