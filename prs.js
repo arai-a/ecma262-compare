f2e635c6c0de031f395627a884e64824e856a10a
arai <arai_a@mac.com>
2016-05-27 00:10:50 +0900

    Update history

--- prs.js
+++ prs.js
@@ -1,26 +1,53 @@
 "use strict";
 var prs = {
+ "576": {
+  "login": "bocoup",
+  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
+  "ref": "proxy-typo",
+  "revs": [
+   "ec60a7d12cf4346a62b4db74620085c3ac736422"
+  ],
+  "title": "[editorial] Correct typos"
+ },
  "545": {
   "login": "jmdyck",
-  "base": "f59a091dc6cf1df97d6449f5a23157b8473954f9",
+  "base": "3c9c962abaa2f887e748e1bc63e6ba78141b9004",
   "ref": "operation_headers",
   "revs": [
-   "7374c339c170cd03f44cb03102a83240f7fc3e38",
-   "013986c167256774f4492e40f335286e25bb69b9",
-   "1be0445cfdd2567ddcdb7a64cd01f6afefdb3fa5",
-   "7bcc2ffe201fae52cee811ea4874ec713706adcd",
-   "0942f93ed50870ddcdbb8c381a06df8f51d3df6a"
+   "b1a0637e36973c5b29d744f608468b349e40c82c",
+   "89c1493e90a30087e8e062b6a28b56b038c8e373",
+   "8eb23bbac4852912d4d991faf50781b7f0683fae",
+   "b241e514aa00eb36d5ccd372b3dd20c979b2124a",
+   "90e026b93be215e354c7bb9e1f57329a30bb1014"
   ],
   "title": "Replace each abstract op's preamble with a structured 'header'."
  },
- "453": {
-  "login": "littledan",
-  "base": "18eb8ee7eaee3c608b13fcc270eef0b7abbd1801",
-  "ref": "fib-dups2",
+ "579": {
+  "login": "bocoup",
+  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
+  "ref": "reference-normal-cptn",
   "revs": [
-   "e97ab859395ab0c5d510e2521d8cd105f4ac1e10"
+   "464f24c3d976190bb789b605ce88e4cafdd5d8c7"
   ],
-  "title": "Normative: Allow duplicate FunctionDeclarations in a block"
+  "title": "[editorial] Assert normal completion value"
+ },
+ "581": {
+  "login": "ljharb",
+  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
+  "ref": "string-padstart",
+  "revs": [
+   "c31a686e653dd23cc23c42cd33164539d6d7a09c"
+  ],
+  "title": "Per May 2016 TC39 consensus, `String#{padStart,padEnd}` is now stage 4!"
+ },
+ "582": {
+  "login": "ljharb",
+  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
+  "ref": "object-getownpropertydescriptors",
+  "revs": [
+   "55ac18633a0e68ba24b1f62288e3c77ddece58bd"
+  ],
+  "title": "Per May 2016 TC39 consensus, `Object.getOwnPropertyDescriptors` is now stage 4!"
  },
  "353": {
   "login": "anba",
@@ -30,16 +57,6 @@ var prs = {
    "ed10f00476c6b0ded127e0aae5ae1f370053cfb6"
   ],
   "title": "Always update object properties if the property descriptor is applicable"
- },
- "553": {
-  "login": "leobalter",
-  "base": "f59a091dc6cf1df97d6449f5a23157b8473954f9",
-  "ref": "ta-slice",
-  "revs": [
-   "062e3a3bc5a7af3abe4a65055e93753098ea784e",
-   "641bce8abf2c94084079717cfdc9977f95a996e7"
-  ],
-  "title": "TypedArrays slice set operation does not throw"
  },
  "522": {
   "login": "lars-t-hansen",
@@ -60,6 +77,15 @@ var prs = {
   ],
   "title": "Unify handling of RegExp CharacterClassEscapes \\w and \\W"
  },
+ "453": {
+  "login": "littledan",
+  "base": "18eb8ee7eaee3c608b13fcc270eef0b7abbd1801",
+  "ref": "fib-dups2",
+  "revs": [
+   "e97ab859395ab0c5d510e2521d8cd105f4ac1e10"
+  ],
+  "title": "Normative: Allow duplicate FunctionDeclarations in a block"
+ },
  "469": {
   "login": "claudepache",
   "base": "18eb8ee7eaee3c608b13fcc270eef0b7abbd1801",
@@ -68,6 +94,15 @@ var prs = {
    "21cc21d19dd7788069c202d6b248c8095707009b"
   ],
   "title": "[[Construct]]: extends the check against non-undefined primitive"
+ },
+ "569": {
+  "login": "BillyZac",
+  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
+  "ref": "master",
+  "revs": [
+   "6e86bb2a254855547a746668fdf5462c8573fd47"
+  ],
+  "title": "Fix typo."
  },
  "410": {
   "login": "leobalter",
@@ -78,6 +113,15 @@ var prs = {
   ],
   "title": "Normalize TypedArray, ArrayBuffer and DataView constructors "
  },
+ "571": {
+  "login": "jmdyck",
+  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
+  "ref": "grammar_params",
+  "revs": [
+   "6504e6648ee1108ae3d19d48c3fcb7e42d23edbd"
+  ],
+  "title": "Editorial: Tweak the syntax of grammatical parameters."
+ },
  "508": {
   "login": "efaust",
   "base": "18eb8ee7eaee3c608b13fcc270eef0b7abbd1801",
@@ -86,5 +130,14 @@ var prs = {
    "974421bb71e0108888e2735507e2d4590fb1dd81"
   ],
   "title": "Allow cross-realm tail calls to consume linear resources."
+ },
+ "575": {
+  "login": "msaboff",
+  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
+  "ref": "inferred-func-name",
+  "revs": [
+   "8e50dbd9f4510d9605db08b1103a328b021a27b0"
+  ],
+  "title": "Proposed changes to inferred Function.name"
  }
 };
