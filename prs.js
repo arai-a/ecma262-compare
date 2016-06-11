e1527b39be598064be2c1b694194c88336fb005d
arai <arai_a@mac.com>
2016-06-11 20:31:45 +0900

    Update history

--- prs.js
+++ prs.js
@@ -1,62 +1,24 @@
 "use strict";
 var prs = {
- "576": {
-  "login": "bocoup",
-  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
-  "ref": "proxy-typo",
-  "revs": [
-   "ec60a7d12cf4346a62b4db74620085c3ac736422"
-  ],
-  "title": "[editorial] Correct typos"
- },
  "545": {
   "login": "jmdyck",
-  "base": "3c9c962abaa2f887e748e1bc63e6ba78141b9004",
+  "base": "1244a0b23f747ba894291a4ea167cacf94dc3332",
   "ref": "operation_headers",
   "revs": [
-   "b1a0637e36973c5b29d744f608468b349e40c82c",
-   "89c1493e90a30087e8e062b6a28b56b038c8e373",
-   "8eb23bbac4852912d4d991faf50781b7f0683fae",
-   "b241e514aa00eb36d5ccd372b3dd20c979b2124a",
-   "90e026b93be215e354c7bb9e1f57329a30bb1014"
+   "4306c7b89f8410314db7ffca363ef0d4e617aec3",
+   "209000557305fb2c1024bd4363bef51de4f78157",
+   "c176bd18a2102ad0443ca0b68efd1dd4fa290112"
   ],
   "title": "Replace each abstract op's preamble with a structured 'header'."
  },
- "579": {
-  "login": "bocoup",
-  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
-  "ref": "reference-normal-cptn",
-  "revs": [
-   "464f24c3d976190bb789b605ce88e4cafdd5d8c7"
-  ],
-  "title": "[editorial] Assert normal completion value"
- },
- "581": {
-  "login": "ljharb",
-  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
-  "ref": "string-padstart",
-  "revs": [
-   "c31a686e653dd23cc23c42cd33164539d6d7a09c"
-  ],
-  "title": "Per May 2016 TC39 consensus, `String#{padStart,padEnd}` is now stage 4!"
- },
- "582": {
-  "login": "ljharb",
-  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
-  "ref": "object-getownpropertydescriptors",
-  "revs": [
-   "55ac18633a0e68ba24b1f62288e3c77ddece58bd"
-  ],
-  "title": "Per May 2016 TC39 consensus, `Object.getOwnPropertyDescriptors` is now stage 4!"
- },
- "353": {
-  "login": "anba",
+ "508": {
+  "login": "efaust",
   "base": "18eb8ee7eaee3c608b13fcc270eef0b7abbd1801",
-  "ref": "define_nan",
+  "ref": "master",
   "revs": [
-   "ed10f00476c6b0ded127e0aae5ae1f370053cfb6"
+   "974421bb71e0108888e2735507e2d4590fb1dd81"
   ],
-  "title": "Always update object properties if the property descriptor is applicable"
+  "title": "Allow cross-realm tail calls to consume linear resources."
  },
  "522": {
   "login": "lars-t-hansen",
@@ -70,21 +32,31 @@ var prs = {
  },
  "525": {
   "login": "msaboff",
-  "base": "18eb8ee7eaee3c608b13fcc270eef0b7abbd1801",
+  "base": "5f7b2a357a45c804d8f16d81dfce5c7470889de0",
   "ref": "regexp-changes",
   "revs": [
-   "101632eca50a655f886c0702ba93dbdb1882cd1c"
+   "ab3380df9d7171ff6b29a0513c4e3924db0195bf",
+   "0d3d71b12ebb27f7c16a2d409da960890d24d2a2"
   ],
-  "title": "Unify handling of RegExp CharacterClassEscapes \\w and \\W"
+  "title": "Unify handling of RegExp CharacterClassEscapes \\w and \\W and Word Asserts \\b and \\B"
  },
- "453": {
-  "login": "littledan",
-  "base": "18eb8ee7eaee3c608b13fcc270eef0b7abbd1801",
-  "ref": "fib-dups2",
+ "593": {
+  "login": "ljharb",
+  "base": "0394e968649410831a06fa55d87cb0b615944791",
+  "ref": "ljharb/gopds_bad_proxies",
   "revs": [
-   "e97ab859395ab0c5d510e2521d8cd105f4ac1e10"
+   "c611481a192ba84b2b1f082d52f0d295388f76e2"
   ],
-  "title": "Normative: Allow duplicate FunctionDeclarations in a block"
+  "title": "Normative: `Object.getOwnPropertyDescriptors` should not create keys for undefined descriptors"
+ },
+ "595": {
+  "login": "jmdyck",
+  "base": "1244a0b23f747ba894291a4ea167cacf94dc3332",
+  "ref": "dots_for_internal_methods",
+  "revs": [
+   "b8efe7d2ff4740f4ba927601515baf260ee7ea3d"
+  ],
+  "title": "Editorial: use dot notation for internal methods"
  },
  "469": {
   "login": "claudepache",
@@ -95,41 +67,24 @@ var prs = {
   ],
   "title": "[[Construct]]: extends the check against non-undefined primitive"
  },
- "569": {
-  "login": "BillyZac",
-  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
-  "ref": "master",
+ "607": {
+  "login": "bocoup",
+  "base": "1244a0b23f747ba894291a4ea167cacf94dc3332",
+  "ref": "b32",
   "revs": [
-   "6e86bb2a254855547a746668fdf5462c8573fd47"
+   "600cbbe58cee5a72c6c65fc63490f08c2c54a486",
+   "c43e0ee0a0c32e90786b063512ed649f54f7dbbb"
   ],
-  "title": "Fix typo."
+  "title": "[editorial] Complete documentation of Annex B extn"
  },
- "410": {
+ "604": {
   "login": "leobalter",
-  "base": "18eb8ee7eaee3c608b13fcc270eef0b7abbd1801",
-  "ref": "arraybufffer",
+  "base": "5f7b2a357a45c804d8f16d81dfce5c7470889de0",
+  "ref": "dataview-getmethods",
   "revs": [
-   "4689245b36c9eafde3b8b28665648f548b600a42"
+   "2051d5ac17e148e879502c709b5e38ae818f9a76"
   ],
-  "title": "Normalize TypedArray, ArrayBuffer and DataView constructors "
- },
- "571": {
-  "login": "jmdyck",
-  "base": "9b49a888e9dfe2667008a01b2754c3662059ae56",
-  "ref": "grammar_params",
-  "revs": [
-   "6504e6648ee1108ae3d19d48c3fcb7e42d23edbd"
-  ],
-  "title": "Editorial: Tweak the syntax of grammatical parameters."
- },
- "508": {
-  "login": "efaust",
-  "base": "18eb8ee7eaee3c608b13fcc270eef0b7abbd1801",
-  "ref": "master",
-  "revs": [
-   "974421bb71e0108888e2735507e2d4590fb1dd81"
-  ],
-  "title": "Allow cross-realm tail calls to consume linear resources."
+  "title": "Normative: DataView methods should throw for absent byteOffset"
  },
  "575": {
   "login": "msaboff",
