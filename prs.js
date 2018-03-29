4b75c8fcd8d52140a5eeedc81560199fe4f4ff19
arai <arai_a@mac.com>
2018-03-29 17:39:31 +0900

    add history

--- prs.js
+++ prs.js
@@ -1,5 +1,14 @@
 "use strict";
 var prs = {
+ "1153": {
+  "login": "ljharb",
+  "base": "4c2df13f4194057f09b920ee88712e5a70b1a556",
+  "ref": "global_object_dfn",
+  "revs": [
+   "1cb97149c6bc3c76aee087be96357acae81714ee"
+  ],
+  "title": "Editorial: Move global object dfn to the Global Object section"
+ },
  "1111": {
   "login": "woess",
   "base": "0e1b4c287d4a8a976bf3396f4d810ddc63d12820",
@@ -8,5 +17,102 @@ var prs = {
    "987266b539cfd9592e109995c8150fcdd1ba2a5f"
   ],
   "title": "Editorial: Add missing return-if-done in ForIn/OfBodyEvaluation"
+ },
+ "1131": {
+  "login": "anba",
+  "base": "0e1b4c287d4a8a976bf3396f4d810ddc63d12820",
+  "ref": "dataview-length",
+  "revs": [
+   "f4d38b0c131a0c7515823476d4c20747e3019dec"
+  ],
+  "title": "Restore original definition of DataView.length"
+ },
+ "1132": {
+  "login": "anba",
+  "base": "0e1b4c287d4a8a976bf3396f4d810ddc63d12820",
+  "ref": "invalid-strict-mode-assign",
+  "revs": [
+   "2272bfb32d03e526ab162d6333ffa32bd58e438f"
+  ],
+  "title": "Restore early SyntaxError for strict mode assignment to eval/arguments"
+ },
+ "1135": {
+  "login": "littledan",
+  "base": "0e1b4c287d4a8a976bf3396f4d810ddc63d12820",
+  "ref": "number-refactoring",
+  "revs": [
+   "52743a22b51581002ee36f294ac46d1318480d93"
+  ],
+  "title": "Editorial: Explicitly note mathematical values"
+ },
+ "1136": {
+  "login": "pemrouz",
+  "base": "0e1b4c287d4a8a976bf3396f4d810ddc63d12820",
+  "ref": "normative/await-in-parameter-defaults",
+  "revs": [
+   "061fc43db42d757d1f862514d2ac637b1733d80b"
+  ],
+  "title": "normative: allow await in parameter defaults (#917)"
+ },
+ "1137": {
+  "login": "bakkot",
+  "base": "0e1b4c287d4a8a976bf3396f4d810ddc63d12820",
+  "ref": "fn-errors",
+  "revs": [
+   "9389a5cf49bde1d01e8c920b3519b4f0bb762869"
+  ],
+  "title": "Editorial: clarify when early errors for functions apply"
+ },
+ "1141": {
+  "login": "ljharb",
+  "base": "81be357744a8c8a9be96ef5559e2a5fa8eb90704",
+  "ref": "fix_async_gen_ids",
+  "revs": [
+   "bd9bc78b0a9f40616e5e4809bf1457583419c22f"
+  ],
+  "title": "Editorial: fix section IDs for some async generator sections."
+ },
+ "1143": {
+  "login": "ljharb",
+  "base": "4c2df13f4194057f09b920ee88712e5a70b1a556",
+  "ref": "punctuation_consistent",
+  "revs": [
+   "5f3a58f5e8df7a15163b42bd3a8d15bf64660762",
+   "2a4cff9f2e39d056366b9d7d0f4def619b09d25c",
+   "7d331cee270f998a16838acd87ab1138736e5bb9",
+   "9628a96ad95023d5d5bff2865dd3a55e884d48db",
+   "9cc087ca67dd12fc848a488c78ce7b177c132dbd",
+   "5e022ae6a3a910b6f3bcdf74008c2d4b04d19b53",
+   "f48b68750d0bcef70c2818a8c68aea8e25e7e32c"
+  ],
+  "title": "Editorial: Make punctuation and spacing more consistent"
+ },
+ "1144": {
+  "login": "apaprocki",
+  "base": "4c2df13f4194057f09b920ee88712e5a70b1a556",
+  "ref": "time-value-range-editorial",
+  "revs": [
+   "d61e1b801fb56e03e9c3193f408297087f805642",
+   "46353f0af79efd89459d9a1ef5465d2ad3f3f970"
+  ],
+  "title": "Editorial: Cleanup Time Values and Time Range"
+ },
+ "1146": {
+  "login": "ljharb",
+  "base": "4c2df13f4194057f09b920ee88712e5a70b1a556",
+  "ref": "promise_undefined_capability",
+  "revs": [
+   "a1234e95cb9e5cffaec43055df02fee63cbc7e31"
+  ],
+  "title": "Editorial: refactor PerformPromiseThen to allow no capability"
+ },
+ "1148": {
+  "login": "ljharb",
+  "base": "4c2df13f4194057f09b920ee88712e5a70b1a556",
+  "ref": "remove_poison_pill_legacy",
+  "revs": [
+   "334007e208f0d464a1bca38adaf717f9e9e503b2"
+  ],
+  "title": "Editorial: remove unnecessary AddRestrictedFunctionProperties"
  }
 };
