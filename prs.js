e3b5b1d5c5cfa4ef7685de84a462a85578248390
Tooru Fujisawa <arai_a@mac.com>
2020-02-12 08:08:10 +0900

    add one PR

--- prs.js
+++ prs.js
@@ -8,5 +8,14 @@ var prs = {
   ],
   "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
   "title": "Editorial: Fix SubstitutionTemplate ArgumentListEvaluation"
+ },
+ "1408": {
+  "ref": "iterator-close-get-method",
+  "login": "zenparsing",
+  "revs": [
+   "f83865d2209a18287e05c8dab578d69ab497c9af"
+  ],
+  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
+  "title": "Normative: Suppress GetMethod errors in IteratorClose"
  }
 };
