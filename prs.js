bfc9a13a27228bb447223f03351d3869e4b78e17
GitHub Action <action@github.com>
2020-02-11 23:50:49 +0000

    update history

--- prs.js
+++ prs.js
@@ -1,5 +1,14 @@
 "use strict";
 var prs = {
+ "1408": {
+  "ref": "iterator-close-get-method",
+  "login": "zenparsing",
+  "revs": [
+   "f83865d2209a18287e05c8dab578d69ab497c9af"
+  ],
+  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
+  "title": "Normative: Suppress GetMethod errors in IteratorClose"
+ },
  "1402": {
   "ref": "template-bugfix",
   "login": "TimothyGu",
@@ -9,13 +18,21 @@ var prs = {
   "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
   "title": "Editorial: Fix SubstitutionTemplate ArgumentListEvaluation"
  },
- "1408": {
-  "ref": "iterator-close-get-method",
-  "login": "zenparsing",
+ "1418": {
+  "ref": "2019-01-explain-SAFE_INTEGER",
+  "login": "gibson042",
   "revs": [
-   "f83865d2209a18287e05c8dab578d69ab497c9af"
+   "a6a7b5069e37dec40fac0c2662f81404a57be29e",
+   "4c3b44b9badbd1ad238a16e251662bc632f401aa",
+   "9c0732843f78d257d63e18189cd5ee020d4dfb1e",
+   "41ddc28da1f0be598c1cdf409b6c24582c685b60",
+   "c62904610cee845a7f77051ef6d315406e29aa66",
+   "9c1ca73a518241918c88ca81e875aa124eff00ad",
+   "ed8a3119bb08fb7198cc9feef622e06261b714dc",
+   "9a914a4d650e5bb8452d348c926bc2334104cee4",
+   "75adffca9c7bc41f252e9386c8ad7deefa3040cf"
   ],
-  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
-  "title": "Normative: Suppress GetMethod errors in IteratorClose"
+  "base": "693e09a4b9ce52b060ceda897b042c3f83f0a738",
+  "title": "Editorial: Explain why *_SAFE_INTEGER magnitude is not a power of two"
  }
 };
