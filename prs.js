87c126d7d33251c541f334fa46f451c26c37f71d
GitHub Action <action@github.com>
2020-02-12 02:39:34 +0000

    update history

--- prs.js
+++ prs.js
@@ -1,112 +1,22 @@
 "use strict";
 var prs = {
- "1402": {
-  "ref": "template-bugfix",
-  "login": "TimothyGu",
+ "1556": {
+  "ref": "typed-array-get-set",
+  "login": "shvaikalesh",
   "revs": [
-   "5a50eca06776073cdabe58d9478d71dddf3e75e0"
+   "80546f0496dafffeed842cb456a5f8693d435069"
   ],
-  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
-  "title": "Editorial: Fix SubstitutionTemplate ArgumentListEvaluation"
+  "base": "a5375bdad264c8aa264d9c44f57408087761069e",
+  "title": "Normative: Guard TypedArray methods from non-exotic receivers"
  },
- "1493": {
-  "ref": "htmlclosecomment",
-  "login": "leobalter",
+ "1518": {
+  "ref": "array_join_cyclic",
+  "login": "ljharb",
   "revs": [
-   "b6cc8db432c117c9dee96a1a9314e56b669fb51f"
+   "dd9d1d2c2246b60b7b1b9dcea353de773ba78598"
   ],
-  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
-  "title": "Normative: Extend specs to observe WebReality for HTMLCloseComment"
- },
- "1522": {
-  "ref": "re_1510",
-  "login": "jmdyck",
-  "revs": [
-   "77e9f1bdc8a9005dde346e8ecc465f4fb8f6d9fb"
-  ],
-  "base": "1595e86b09414b669fcdd23e576ec912685d5f2b",
-  "title": "Editorial: Clarify and expand the discussion of terminal symbols"
- },
- "1547": {
-  "ref": "SourceText",
-  "login": "jmdyck",
-  "revs": [
-   "0cccdeacf29719969e1749f6078b8c8ef9550d10"
-  ],
-  "base": "787642ad2d159c8358a8782c9414f6d5fb6efa6f",
-  "title": "Editorial: fix inconsistency re type of [[SourceText]]"
- },
- "1554": {
-  "ref": "NumericValue",
-  "login": "jmdyck",
-  "revs": [
-   "86ded1df9ac36aa4ec954a8d1ccbe7d47e117d49"
-  ],
-  "base": "1595e86b09414b669fcdd23e576ec912685d5f2b",
-  "title": "Editorial: Define + use StringToNumber, and better define NumericValue"
- },
- "1564": {
-  "ref": "gh-1087-time-arithmetic-precision",
-  "login": "gibson042",
-  "revs": [
-   "88c606ebf48faeba45eca5bd1b4c7d6d1e71c45c"
-  ],
-  "base": "693e09a4b9ce52b060ceda897b042c3f83f0a738",
-  "title": "Normative: Use Number arithmetic for constructing dates and times"
- },
- "1501": {
-  "ref": "master",
-  "login": "dandclark",
-  "revs": [
-   "85105c567933960c4993b50643810526c7f05eee"
-  ],
-  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
-  "title": "Layering: Add GetRequestedModules() to Module Record"
- },
- "1420": {
-  "ref": "parameterize-intrinsics",
-  "login": "bmeck",
-  "revs": [
-   "a2b0457bb1560d1674d624b4233d499df324a7fb"
-  ],
-  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
-  "title": "Allow hosts to reuse an existing Realm Intrinsics record"
- },
- "1418": {
-  "ref": "2019-01-explain-SAFE_INTEGER",
-  "login": "gibson042",
-  "revs": [
-   "a6a7b5069e37dec40fac0c2662f81404a57be29e"
-  ],
-  "base": "693e09a4b9ce52b060ceda897b042c3f83f0a738",
-  "title": "Editorial: Explain why *_SAFE_INTEGER magnitude is not a power of two"
- },
- "1498": {
-  "ref": "pr-needs-consensus-hostensurecancompilestrings-passthru",
-  "login": "tc39",
-  "revs": [
-   "e344423989fe2599f5ffc86065ac12c28176170e"
-  ],
-  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
-  "title": "Provide HostEnsureCanCompileString implementors more context"
- },
- "1438": {
-  "ref": "object-prop-has",
-  "login": "bmeck",
-  "revs": [
-   "45b775fc0aefce68aaab7f1f242a6373ce2f7584"
-  ],
-  "base": "257cae9f69068704fc9e89a729f54bd7977c9161",
-  "title": "Editorial: Add missing has operation to property operations"
- },
- "1463": {
-  "ref": "master",
-  "login": "viktmv",
-  "revs": [
-   "343b55373369a219086fd1fe1cead3dfc1122500"
-  ],
-  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
-  "title": "Editorial: Fix description of [[ThisMode]] internal slot of Function Object"
+  "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
+  "title": "Normative: specify web reality for Array.prototype.join with cyclic values"
  },
  "1490": {
   "ref": "anon-empty-name",
@@ -126,14 +36,23 @@ var prs = {
   "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
   "title": "Normative: Suppress GetMethod errors in IteratorClose"
  },
- "1518": {
-  "ref": "array_join_cyclic",
-  "login": "ljharb",
+ "1554": {
+  "ref": "NumericValue",
+  "login": "jmdyck",
   "revs": [
-   "dd9d1d2c2246b60b7b1b9dcea353de773ba78598"
+   "86ded1df9ac36aa4ec954a8d1ccbe7d47e117d49"
   ],
-  "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
-  "title": "Normative: specify web reality for Array.prototype.join with cyclic values"
+  "base": "1595e86b09414b669fcdd23e576ec912685d5f2b",
+  "title": "Editorial: Define + use StringToNumber, and better define NumericValue"
+ },
+ "1463": {
+  "ref": "master",
+  "login": "viktmv",
+  "revs": [
+   "343b55373369a219086fd1fe1cead3dfc1122500"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Editorial: Fix description of [[ThisMode]] internal slot of Function Object"
  },
  "1517": {
   "ref": "fix_matchall",
@@ -144,6 +63,33 @@ var prs = {
   "base": "f158df16b4bfa4d8d1efa7407ebfe8eeb339f4fd",
   "title": "Normative: correct spec bug in `RegExp.prototype[Symbol.matchAll]`"
  },
+ "1501": {
+  "ref": "master",
+  "login": "dandclark",
+  "revs": [
+   "85105c567933960c4993b50643810526c7f05eee"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Layering: Add GetRequestedModules() to Module Record"
+ },
+ "1570": {
+  "ref": "returnifabrupt",
+  "login": "michaelficarra",
+  "revs": [
+   "e42a39ac4b59e5aa62fc83726df2aa4b60d7b1e0"
+  ],
+  "base": "c808fe2b7bcc71ff22b06fe96b4260edcc4be5ba",
+  "title": "Editorial: remove useless step of ReturnIfAbrupt expansion"
+ },
+ "1547": {
+  "ref": "SourceText",
+  "login": "jmdyck",
+  "revs": [
+   "0cccdeacf29719969e1749f6078b8c8ef9550d10"
+  ],
+  "base": "787642ad2d159c8358a8782c9414f6d5fb6efa6f",
+  "title": "Editorial: fix inconsistency re type of [[SourceText]]"
+ },
  "1519": {
   "ref": "resolve_831",
   "login": "jmdyck",
@@ -153,13 +99,112 @@ var prs = {
   "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
   "title": "Editorial: Remove unused steps from definitions of Contains"
  },
- "1556": {
-  "ref": "typed-array-get-set",
-  "login": "shvaikalesh",
+ "1573": {
+  "ref": "returnifabrupt-refactor",
+  "login": "michaelficarra",
   "revs": [
-   "80546f0496dafffeed842cb456a5f8693d435069"
+   "ea5d20db0396bf27079fb33537e01dd75ee41ad7"
   ],
-  "base": "a5375bdad264c8aa264d9c44f57408087761069e",
-  "title": "Normative: Guard TypedArray methods from non-exotic receivers"
+  "base": "a25df663ddaa8f0b976f0411681635f587be63e0",
+  "title": "change how ReturnIfAbrupt and its shorthands are specified"
+ },
+ "1493": {
+  "ref": "htmlclosecomment",
+  "login": "leobalter",
+  "revs": [
+   "b6cc8db432c117c9dee96a1a9314e56b669fb51f"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Normative: Extend specs to observe WebReality for HTMLCloseComment"
+ },
+ "1564": {
+  "ref": "gh-1087-time-arithmetic-precision",
+  "login": "gibson042",
+  "revs": [
+   "88c606ebf48faeba45eca5bd1b4c7d6d1e71c45c"
+  ],
+  "base": "693e09a4b9ce52b060ceda897b042c3f83f0a738",
+  "title": "Normative: Use Number arithmetic for constructing dates and times"
+ },
+ "1402": {
+  "ref": "template-bugfix",
+  "login": "TimothyGu",
+  "revs": [
+   "5a50eca06776073cdabe58d9478d71dddf3e75e0"
+  ],
+  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
+  "title": "Editorial: Fix SubstitutionTemplate ArgumentListEvaluation"
+ },
+ "1598": {
+  "ref": "note-re-cache-deletion",
+  "login": "mikesamuel",
+  "revs": [
+   "306e0b43b860144000cd79c5954b3832ab1a34b5"
+  ],
+  "base": "a95e95a63bc1fd7d71f089ad1d68be0cce4caf34",
+  "title": "Editorial: Provide context for HostImportModuleDynamically requirement"
+ },
+ "1420": {
+  "ref": "parameterize-intrinsics",
+  "login": "bmeck",
+  "revs": [
+   "a2b0457bb1560d1674d624b4233d499df324a7fb"
+  ],
+  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
+  "title": "Allow hosts to reuse an existing Realm Intrinsics record"
+ },
+ "1498": {
+  "ref": "pr-needs-consensus-hostensurecancompilestrings-passthru",
+  "login": "tc39",
+  "revs": [
+   "e344423989fe2599f5ffc86065ac12c28176170e"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Provide HostEnsureCanCompileString implementors more context"
+ },
+ "1585": {
+  "ref": "array-sort",
+  "login": "szuend",
+  "revs": [
+   "09445f5630acc2d3c47a77195cf3e059606ee18b"
+  ],
+  "base": "e3707ac9e14b75b9513d6b09c394dee6473c5ddf",
+  "title": "Normative: A more precise Array.prototype.sort"
+ },
+ "1418": {
+  "ref": "2019-01-explain-SAFE_INTEGER",
+  "login": "gibson042",
+  "revs": [
+   "a6a7b5069e37dec40fac0c2662f81404a57be29e"
+  ],
+  "base": "693e09a4b9ce52b060ceda897b042c3f83f0a738",
+  "title": "Editorial: Explain why *_SAFE_INTEGER magnitude is not a power of two"
+ },
+ "1438": {
+  "ref": "object-prop-has",
+  "login": "bmeck",
+  "revs": [
+   "45b775fc0aefce68aaab7f1f242a6373ce2f7584"
+  ],
+  "base": "257cae9f69068704fc9e89a729f54bd7977c9161",
+  "title": "Editorial: Add missing has operation to property operations"
+ },
+ "1522": {
+  "ref": "re_1510",
+  "login": "jmdyck",
+  "revs": [
+   "77e9f1bdc8a9005dde346e8ecc465f4fb8f6d9fb"
+  ],
+  "base": "1595e86b09414b669fcdd23e576ec912685d5f2b",
+  "title": "Editorial: Clarify and expand the discussion of terminal symbols"
+ },
+ "1597": {
+  "ref": "enqueue-promise-job",
+  "login": "littledan",
+  "revs": [
+   "6d7c4dc4d2d9bfda2b9ecb74ef8009b78f414770"
+  ],
+  "base": "788736c4764901fb25d20ced12d5ac2e10957dc9",
+  "title": "Layering: Allow hosts to cleanly specify Promise Job semantics"
  }
 };
