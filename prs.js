77ae3905b6b08d05d45798cafe7f3159712150d6
Tooru Fujisawa <arai_a@mac.com>
2020-02-12 11:20:31 +0900

    Remove non-topmost revisions for PR

--- prs.js
+++ prs.js
@@ -1,82 +1,32 @@
 "use strict";
 var prs = {
- "1556": {
-  "ref": "typed-array-get-set",
-  "login": "shvaikalesh",
+ "1402": {
+  "ref": "template-bugfix",
+  "login": "TimothyGu",
   "revs": [
-   "80546f0496dafffeed842cb456a5f8693d435069"
-  ],
-  "base": "a5375bdad264c8aa264d9c44f57408087761069e",
-  "title": "Normative: Guard TypedArray methods from non-exotic receivers"
- },
- "1518": {
-  "ref": "array_join_cyclic",
-  "login": "ljharb",
-  "revs": [
-   "dd9d1d2c2246b60b7b1b9dcea353de773ba78598"
-  ],
-  "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
-  "title": "Normative: specify web reality for Array.prototype.join with cyclic values"
- },
- "1490": {
-  "ref": "anon-empty-name",
-  "login": "anba",
-  "revs": [
-   "b3bfef067a59548ef763903a4e59ef1891508bc0",
-   "36a81a0b5eb1a4aa5a3a5a59dadabdbc8717c228",
-   "6ee249e53b02f76f1a09c2d0a7e5ad3c218fb558"
-  ],
-  "base": "8f0f69279b5f9e12ebe8f7052322b93d17e36b54",
-  "title": "Set \"name\" property for anonymous functions"
- },
- "1408": {
-  "ref": "iterator-close-get-method",
-  "login": "zenparsing",
-  "revs": [
-   "f83865d2209a18287e05c8dab578d69ab497c9af"
+   "5a50eca06776073cdabe58d9478d71dddf3e75e0"
   ],
   "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
-  "title": "Normative: Suppress GetMethod errors in IteratorClose"
+  "title": "Editorial: Fix SubstitutionTemplate ArgumentListEvaluation"
  },
- "1554": {
-  "ref": "NumericValue",
+ "1493": {
+  "ref": "htmlclosecomment",
+  "login": "leobalter",
+  "revs": [
+   "b6cc8db432c117c9dee96a1a9314e56b669fb51f"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Normative: Extend specs to observe WebReality for HTMLCloseComment"
+ },
+ "1522": {
+  "ref": "re_1510",
   "login": "jmdyck",
   "revs": [
-   "86ded1df9ac36aa4ec954a8d1ccbe7d47e117d49"
+   "77e9f1bdc8a9005dde346e8ecc465f4fb8f6d9fb",
+   "759402f8801efc5a60e6119086a9844813556bdd"
   ],
   "base": "1595e86b09414b669fcdd23e576ec912685d5f2b",
-  "title": "Editorial: Define + use StringToNumber, and better define NumericValue"
- },
- "1463": {
-  "ref": "master",
-  "login": "viktmv",
-  "revs": [
-   "343b55373369a219086fd1fe1cead3dfc1122500",
-   "6cb3e528bf6cccbc8ff907593e2fe6ec9d522f84"
-  ],
-  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
-  "title": "Editorial: Fix description of [[ThisMode]] internal slot of Function Object"
- },
- "1517": {
-  "ref": "fix_matchall",
-  "login": "ljharb",
-  "revs": [
-   "7244f340581a62d1bb4b1c009cef7564206777ac"
-  ],
-  "base": "f158df16b4bfa4d8d1efa7407ebfe8eeb339f4fd",
-  "title": "Normative: correct spec bug in `RegExp.prototype[Symbol.matchAll]`"
- },
- "1501": {
-  "ref": "master",
-  "login": "dandclark",
-  "revs": [
-   "85105c567933960c4993b50643810526c7f05eee",
-   "6a3967534fad0f56a7cb701663ed65ea9a29c6f2",
-   "1b8e7f5137338d82db141a67d6fdaede002dfe4e",
-   "10dfe19117347524e507e7d1c04e0f4b00fa74d2"
-  ],
-  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
-  "title": "Layering: Add GetRequestedModules() to Module Record"
+  "title": "Editorial: Clarify and expand the discussion of terminal symbols"
  },
  "1547": {
   "ref": "SourceText",
@@ -87,24 +37,14 @@ var prs = {
   "base": "787642ad2d159c8358a8782c9414f6d5fb6efa6f",
   "title": "Editorial: fix inconsistency re type of [[SourceText]]"
  },
- "1519": {
-  "ref": "resolve_831",
+ "1554": {
+  "ref": "NumericValue",
   "login": "jmdyck",
   "revs": [
-   "4b2517039e53c7cea471a4bc432d883d8c5f6683",
-   "b6f3f5e81f04548c67c24cd319a6909e224da7f4"
+   "86ded1df9ac36aa4ec954a8d1ccbe7d47e117d49"
   ],
-  "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
-  "title": "Editorial: Remove unused steps from definitions of Contains"
- },
- "1493": {
-  "ref": "htmlclosecomment",
-  "login": "leobalter",
-  "revs": [
-   "b6cc8db432c117c9dee96a1a9314e56b669fb51f"
-  ],
-  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
-  "title": "Normative: Extend specs to observe WebReality for HTMLCloseComment"
+  "base": "1595e86b09414b669fcdd23e576ec912685d5f2b",
+  "title": "Editorial: Define + use StringToNumber, and better define NumericValue"
  },
  "1564": {
   "ref": "gh-1087-time-arithmetic-precision",
@@ -126,14 +66,17 @@ var prs = {
   "base": "693e09a4b9ce52b060ceda897b042c3f83f0a738",
   "title": "Normative: Use Number arithmetic for constructing dates and times"
  },
- "1402": {
-  "ref": "template-bugfix",
-  "login": "TimothyGu",
+ "1501": {
+  "ref": "master",
+  "login": "dandclark",
   "revs": [
-   "5a50eca06776073cdabe58d9478d71dddf3e75e0"
+   "85105c567933960c4993b50643810526c7f05eee",
+   "6a3967534fad0f56a7cb701663ed65ea9a29c6f2",
+   "1b8e7f5137338d82db141a67d6fdaede002dfe4e",
+   "10dfe19117347524e507e7d1c04e0f4b00fa74d2"
   ],
-  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
-  "title": "Editorial: Fix SubstitutionTemplate ArgumentListEvaluation"
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Layering: Add GetRequestedModules() to Module Record"
  },
  "1420": {
   "ref": "parameterize-intrinsics",
@@ -144,6 +87,15 @@ var prs = {
   "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
   "title": "Allow hosts to reuse an existing Realm Intrinsics record"
  },
+ "1418": {
+  "ref": "2019-01-explain-SAFE_INTEGER",
+  "login": "gibson042",
+  "revs": [
+   "a6a7b5069e37dec40fac0c2662f81404a57be29e"
+  ],
+  "base": "693e09a4b9ce52b060ceda897b042c3f83f0a738",
+  "title": "Editorial: Explain why *_SAFE_INTEGER magnitude is not a power of two"
+ },
  "1498": {
   "ref": "pr-needs-consensus-hostensurecancompilestrings-passthru",
   "login": "tc39",
@@ -152,23 +104,6 @@ var prs = {
   ],
   "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
   "title": "Provide HostEnsureCanCompileString implementors more context"
- },
- "1418": {
-  "ref": "2019-01-explain-SAFE_INTEGER",
-  "login": "gibson042",
-  "revs": [
-   "a6a7b5069e37dec40fac0c2662f81404a57be29e",
-   "4c3b44b9badbd1ad238a16e251662bc632f401aa",
-   "9c0732843f78d257d63e18189cd5ee020d4dfb1e",
-   "41ddc28da1f0be598c1cdf409b6c24582c685b60",
-   "c62904610cee845a7f77051ef6d315406e29aa66",
-   "9c1ca73a518241918c88ca81e875aa124eff00ad",
-   "ed8a3119bb08fb7198cc9feef622e06261b714dc",
-   "9a914a4d650e5bb8452d348c926bc2334104cee4",
-   "75adffca9c7bc41f252e9386c8ad7deefa3040cf"
-  ],
-  "base": "693e09a4b9ce52b060ceda897b042c3f83f0a738",
-  "title": "Editorial: Explain why *_SAFE_INTEGER magnitude is not a power of two"
  },
  "1438": {
   "ref": "object-prop-has",
@@ -179,14 +114,71 @@ var prs = {
   "base": "257cae9f69068704fc9e89a729f54bd7977c9161",
   "title": "Editorial: Add missing has operation to property operations"
  },
- "1522": {
-  "ref": "re_1510",
+ "1463": {
+  "ref": "master",
+  "login": "viktmv",
+  "revs": [
+   "343b55373369a219086fd1fe1cead3dfc1122500",
+   "6cb3e528bf6cccbc8ff907593e2fe6ec9d522f84"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Editorial: Fix description of [[ThisMode]] internal slot of Function Object"
+ },
+ "1490": {
+  "ref": "anon-empty-name",
+  "login": "anba",
+  "revs": [
+   "b3bfef067a59548ef763903a4e59ef1891508bc0",
+   "36a81a0b5eb1a4aa5a3a5a59dadabdbc8717c228",
+   "6ee249e53b02f76f1a09c2d0a7e5ad3c218fb558"
+  ],
+  "base": "8f0f69279b5f9e12ebe8f7052322b93d17e36b54",
+  "title": "Set \"name\" property for anonymous functions"
+ },
+ "1408": {
+  "ref": "iterator-close-get-method",
+  "login": "zenparsing",
+  "revs": [
+   "f83865d2209a18287e05c8dab578d69ab497c9af"
+  ],
+  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
+  "title": "Normative: Suppress GetMethod errors in IteratorClose"
+ },
+ "1518": {
+  "ref": "array_join_cyclic",
+  "login": "ljharb",
+  "revs": [
+   "dd9d1d2c2246b60b7b1b9dcea353de773ba78598"
+  ],
+  "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
+  "title": "Normative: specify web reality for Array.prototype.join with cyclic values"
+ },
+ "1517": {
+  "ref": "fix_matchall",
+  "login": "ljharb",
+  "revs": [
+   "7244f340581a62d1bb4b1c009cef7564206777ac"
+  ],
+  "base": "f158df16b4bfa4d8d1efa7407ebfe8eeb339f4fd",
+  "title": "Normative: correct spec bug in `RegExp.prototype[Symbol.matchAll]`"
+ },
+ "1519": {
+  "ref": "resolve_831",
   "login": "jmdyck",
   "revs": [
-   "77e9f1bdc8a9005dde346e8ecc465f4fb8f6d9fb",
-   "759402f8801efc5a60e6119086a9844813556bdd"
+   "4b2517039e53c7cea471a4bc432d883d8c5f6683",
+   "b6f3f5e81f04548c67c24cd319a6909e224da7f4"
   ],
-  "base": "1595e86b09414b669fcdd23e576ec912685d5f2b",
-  "title": "Editorial: Clarify and expand the discussion of terminal symbols"
+  "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
+  "title": "Editorial: Remove unused steps from definitions of Contains"
+ },
+ "1556": {
+  "ref": "typed-array-get-set",
+  "login": "shvaikalesh",
+  "revs": [
+   "80546f0496dafffeed842cb456a5f8693d435069"
+  ],
+  "base": "a5375bdad264c8aa264d9c44f57408087761069e",
+  "title": "Normative: Guard TypedArray methods from non-exotic receivers"
  }
 };
