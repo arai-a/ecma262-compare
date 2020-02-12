84d38db48e4ed5905c1824ca335a6003e3dd44bc
Tooru Fujisawa <arai_a@mac.com>
2020-02-12 11:22:10 +0900

    Reflect to prs.js

--- prs.js
+++ prs.js
@@ -22,8 +22,7 @@ var prs = {
   "ref": "re_1510",
   "login": "jmdyck",
   "revs": [
-   "77e9f1bdc8a9005dde346e8ecc465f4fb8f6d9fb",
-   "759402f8801efc5a60e6119086a9844813556bdd"
+   "77e9f1bdc8a9005dde346e8ecc465f4fb8f6d9fb"
   ],
   "base": "1595e86b09414b669fcdd23e576ec912685d5f2b",
   "title": "Editorial: Clarify and expand the discussion of terminal symbols"
@@ -50,18 +49,7 @@ var prs = {
   "ref": "gh-1087-time-arithmetic-precision",
   "login": "gibson042",
   "revs": [
-   "88c606ebf48faeba45eca5bd1b4c7d6d1e71c45c",
-   "4facda5050d042c77099bb7d5aa50eea11e77e0e",
-   "0b55c599d3b5b7f0d1fe93b414fbf77a40a690c2",
-   "e4b9b2d7b664f51e0c6fe1b0e7e713cb907aa770",
-   "7f6553f8330365313a1bb24a3f9c0abfdfcd8821",
-   "1dde075353b1eea89225cea063aa229434333f63",
-   "ad9cfff82fecc900cc010f4b44fa0c7407dc027a",
-   "ee9f5935d3a6cb0eed6834996a57c52de9391ff4",
-   "010b3624d43b02d1c97324bbbdb511860e12488d",
-   "0a64fb44af6961c2a1b208a56e815c7dec759671",
-   "a43674cc7b4080f72f0708eeee43e60c1ac8da70",
-   "e57ab2fc22fed86fc8c9a9c6c08366505c9a85f2"
+   "88c606ebf48faeba45eca5bd1b4c7d6d1e71c45c"
   ],
   "base": "693e09a4b9ce52b060ceda897b042c3f83f0a738",
   "title": "Normative: Use Number arithmetic for constructing dates and times"
@@ -70,10 +58,7 @@ var prs = {
   "ref": "master",
   "login": "dandclark",
   "revs": [
-   "85105c567933960c4993b50643810526c7f05eee",
-   "6a3967534fad0f56a7cb701663ed65ea9a29c6f2",
-   "1b8e7f5137338d82db141a67d6fdaede002dfe4e",
-   "10dfe19117347524e507e7d1c04e0f4b00fa74d2"
+   "85105c567933960c4993b50643810526c7f05eee"
   ],
   "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
   "title": "Layering: Add GetRequestedModules() to Module Record"
@@ -118,8 +103,7 @@ var prs = {
   "ref": "master",
   "login": "viktmv",
   "revs": [
-   "343b55373369a219086fd1fe1cead3dfc1122500",
-   "6cb3e528bf6cccbc8ff907593e2fe6ec9d522f84"
+   "343b55373369a219086fd1fe1cead3dfc1122500"
   ],
   "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
   "title": "Editorial: Fix description of [[ThisMode]] internal slot of Function Object"
@@ -128,9 +112,7 @@ var prs = {
   "ref": "anon-empty-name",
   "login": "anba",
   "revs": [
-   "b3bfef067a59548ef763903a4e59ef1891508bc0",
-   "36a81a0b5eb1a4aa5a3a5a59dadabdbc8717c228",
-   "6ee249e53b02f76f1a09c2d0a7e5ad3c218fb558"
+   "b3bfef067a59548ef763903a4e59ef1891508bc0"
   ],
   "base": "8f0f69279b5f9e12ebe8f7052322b93d17e36b54",
   "title": "Set \"name\" property for anonymous functions"
@@ -166,8 +148,7 @@ var prs = {
   "ref": "resolve_831",
   "login": "jmdyck",
   "revs": [
-   "4b2517039e53c7cea471a4bc432d883d8c5f6683",
-   "b6f3f5e81f04548c67c24cd319a6909e224da7f4"
+   "4b2517039e53c7cea471a4bc432d883d8c5f6683"
   ],
   "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
   "title": "Editorial: Remove unused steps from definitions of Contains"
