17d3e2526ca3f821fea7161b95fe2c0b503be33a
Tooru Fujisawa <arai_a@mac.com>
2020-02-12 09:19:36 +0900

    add some PRs

--- prs.js
+++ prs.js
@@ -1,14 +1,5 @@
 "use strict";
 var prs = {
- "1408": {
-  "ref": "iterator-close-get-method",
-  "login": "zenparsing",
-  "revs": [
-   "f83865d2209a18287e05c8dab578d69ab497c9af"
-  ],
-  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
-  "title": "Normative: Suppress GetMethod errors in IteratorClose"
- },
  "1402": {
   "ref": "template-bugfix",
   "login": "TimothyGu",
@@ -17,6 +8,36 @@ var prs = {
   ],
   "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
   "title": "Editorial: Fix SubstitutionTemplate ArgumentListEvaluation"
+ },
+ "1493": {
+  "ref": "htmlclosecomment",
+  "login": "leobalter",
+  "revs": [
+   "b6cc8db432c117c9dee96a1a9314e56b669fb51f"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Normative: Extend specs to observe WebReality for HTMLCloseComment"
+ },
+ "1501": {
+  "ref": "master",
+  "login": "dandclark",
+  "revs": [
+   "85105c567933960c4993b50643810526c7f05eee",
+   "6a3967534fad0f56a7cb701663ed65ea9a29c6f2",
+   "1b8e7f5137338d82db141a67d6fdaede002dfe4e",
+   "10dfe19117347524e507e7d1c04e0f4b00fa74d2"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Layering: Add GetRequestedModules() to Module Record"
+ },
+ "1420": {
+  "ref": "parameterize-intrinsics",
+  "login": "bmeck",
+  "revs": [
+   "a2b0457bb1560d1674d624b4233d499df324a7fb"
+  ],
+  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
+  "title": "Allow hosts to reuse an existing Realm Intrinsics record"
  },
  "1418": {
   "ref": "2019-01-explain-SAFE_INTEGER",
@@ -34,5 +55,81 @@ var prs = {
   ],
   "base": "693e09a4b9ce52b060ceda897b042c3f83f0a738",
   "title": "Editorial: Explain why *_SAFE_INTEGER magnitude is not a power of two"
+ },
+ "1498": {
+  "ref": "pr-needs-consensus-hostensurecancompilestrings-passthru",
+  "login": "tc39",
+  "revs": [
+   "e344423989fe2599f5ffc86065ac12c28176170e"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Provide HostEnsureCanCompileString implementors more context"
+ },
+ "1438": {
+  "ref": "object-prop-has",
+  "login": "bmeck",
+  "revs": [
+   "45b775fc0aefce68aaab7f1f242a6373ce2f7584"
+  ],
+  "base": "257cae9f69068704fc9e89a729f54bd7977c9161",
+  "title": "Editorial: Add missing has operation to property operations"
+ },
+ "1463": {
+  "ref": "master",
+  "login": "viktmv",
+  "revs": [
+   "343b55373369a219086fd1fe1cead3dfc1122500",
+   "6cb3e528bf6cccbc8ff907593e2fe6ec9d522f84"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Editorial: Fix description of [[ThisMode]] internal slot of Function Object"
+ },
+ "1490": {
+  "ref": "anon-empty-name",
+  "login": "anba",
+  "revs": [
+   "b3bfef067a59548ef763903a4e59ef1891508bc0",
+   "36a81a0b5eb1a4aa5a3a5a59dadabdbc8717c228",
+   "6ee249e53b02f76f1a09c2d0a7e5ad3c218fb558"
+  ],
+  "base": "8f0f69279b5f9e12ebe8f7052322b93d17e36b54",
+  "title": "Set \"name\" property for anonymous functions"
+ },
+ "1408": {
+  "ref": "iterator-close-get-method",
+  "login": "zenparsing",
+  "revs": [
+   "f83865d2209a18287e05c8dab578d69ab497c9af"
+  ],
+  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
+  "title": "Normative: Suppress GetMethod errors in IteratorClose"
+ },
+ "1518": {
+  "ref": "array_join_cyclic",
+  "login": "ljharb",
+  "revs": [
+   "dd9d1d2c2246b60b7b1b9dcea353de773ba78598"
+  ],
+  "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
+  "title": "Normative: specify web reality for Array.prototype.join with cyclic values"
+ },
+ "1517": {
+  "ref": "fix_matchall",
+  "login": "ljharb",
+  "revs": [
+   "7244f340581a62d1bb4b1c009cef7564206777ac"
+  ],
+  "base": "f158df16b4bfa4d8d1efa7407ebfe8eeb339f4fd",
+  "title": "Normative: correct spec bug in `RegExp.prototype[Symbol.matchAll]`"
+ },
+ "1519": {
+  "ref": "resolve_831",
+  "login": "jmdyck",
+  "revs": [
+   "4b2517039e53c7cea471a4bc432d883d8c5f6683",
+   "b6f3f5e81f04548c67c24cd319a6909e224da7f4"
+  ],
+  "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
+  "title": "Editorial: Remove unused steps from definitions of Contains"
  }
 };
