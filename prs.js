8e439cbb408679f4ca20c6ca9800a8865c53dc9b
GitHub Action <action@github.com>
2020-02-12 00:48:36 +0000

    update history

--- prs.js
+++ prs.js
@@ -1,88 +1,13 @@
 "use strict";
 var prs = {
- "1402": {
-  "ref": "template-bugfix",
-  "login": "TimothyGu",
+ "1518": {
+  "ref": "array_join_cyclic",
+  "login": "ljharb",
   "revs": [
-   "5a50eca06776073cdabe58d9478d71dddf3e75e0"
+   "dd9d1d2c2246b60b7b1b9dcea353de773ba78598"
   ],
-  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
-  "title": "Editorial: Fix SubstitutionTemplate ArgumentListEvaluation"
- },
- "1493": {
-  "ref": "htmlclosecomment",
-  "login": "leobalter",
-  "revs": [
-   "b6cc8db432c117c9dee96a1a9314e56b669fb51f"
-  ],
-  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
-  "title": "Normative: Extend specs to observe WebReality for HTMLCloseComment"
- },
- "1501": {
-  "ref": "master",
-  "login": "dandclark",
-  "revs": [
-   "85105c567933960c4993b50643810526c7f05eee",
-   "6a3967534fad0f56a7cb701663ed65ea9a29c6f2",
-   "1b8e7f5137338d82db141a67d6fdaede002dfe4e",
-   "10dfe19117347524e507e7d1c04e0f4b00fa74d2"
-  ],
-  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
-  "title": "Layering: Add GetRequestedModules() to Module Record"
- },
- "1420": {
-  "ref": "parameterize-intrinsics",
-  "login": "bmeck",
-  "revs": [
-   "a2b0457bb1560d1674d624b4233d499df324a7fb"
-  ],
-  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
-  "title": "Allow hosts to reuse an existing Realm Intrinsics record"
- },
- "1418": {
-  "ref": "2019-01-explain-SAFE_INTEGER",
-  "login": "gibson042",
-  "revs": [
-   "a6a7b5069e37dec40fac0c2662f81404a57be29e",
-   "4c3b44b9badbd1ad238a16e251662bc632f401aa",
-   "9c0732843f78d257d63e18189cd5ee020d4dfb1e",
-   "41ddc28da1f0be598c1cdf409b6c24582c685b60",
-   "c62904610cee845a7f77051ef6d315406e29aa66",
-   "9c1ca73a518241918c88ca81e875aa124eff00ad",
-   "ed8a3119bb08fb7198cc9feef622e06261b714dc",
-   "9a914a4d650e5bb8452d348c926bc2334104cee4",
-   "75adffca9c7bc41f252e9386c8ad7deefa3040cf"
-  ],
-  "base": "693e09a4b9ce52b060ceda897b042c3f83f0a738",
-  "title": "Editorial: Explain why *_SAFE_INTEGER magnitude is not a power of two"
- },
- "1498": {
-  "ref": "pr-needs-consensus-hostensurecancompilestrings-passthru",
-  "login": "tc39",
-  "revs": [
-   "e344423989fe2599f5ffc86065ac12c28176170e"
-  ],
-  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
-  "title": "Provide HostEnsureCanCompileString implementors more context"
- },
- "1438": {
-  "ref": "object-prop-has",
-  "login": "bmeck",
-  "revs": [
-   "45b775fc0aefce68aaab7f1f242a6373ce2f7584"
-  ],
-  "base": "257cae9f69068704fc9e89a729f54bd7977c9161",
-  "title": "Editorial: Add missing has operation to property operations"
- },
- "1463": {
-  "ref": "master",
-  "login": "viktmv",
-  "revs": [
-   "343b55373369a219086fd1fe1cead3dfc1122500",
-   "6cb3e528bf6cccbc8ff907593e2fe6ec9d522f84"
-  ],
-  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
-  "title": "Editorial: Fix description of [[ThisMode]] internal slot of Function Object"
+  "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
+  "title": "Normative: specify web reality for Array.prototype.join with cyclic values"
  },
  "1490": {
   "ref": "anon-empty-name",
@@ -104,14 +29,15 @@ var prs = {
   "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
   "title": "Normative: Suppress GetMethod errors in IteratorClose"
  },
- "1518": {
-  "ref": "array_join_cyclic",
-  "login": "ljharb",
+ "1463": {
+  "ref": "master",
+  "login": "viktmv",
   "revs": [
-   "dd9d1d2c2246b60b7b1b9dcea353de773ba78598"
+   "343b55373369a219086fd1fe1cead3dfc1122500",
+   "6cb3e528bf6cccbc8ff907593e2fe6ec9d522f84"
   ],
-  "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
-  "title": "Normative: specify web reality for Array.prototype.join with cyclic values"
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Editorial: Fix description of [[ThisMode]] internal slot of Function Object"
  },
  "1517": {
   "ref": "fix_matchall",
@@ -122,6 +48,18 @@ var prs = {
   "base": "f158df16b4bfa4d8d1efa7407ebfe8eeb339f4fd",
   "title": "Normative: correct spec bug in `RegExp.prototype[Symbol.matchAll]`"
  },
+ "1501": {
+  "ref": "master",
+  "login": "dandclark",
+  "revs": [
+   "85105c567933960c4993b50643810526c7f05eee",
+   "6a3967534fad0f56a7cb701663ed65ea9a29c6f2",
+   "1b8e7f5137338d82db141a67d6fdaede002dfe4e",
+   "10dfe19117347524e507e7d1c04e0f4b00fa74d2"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Layering: Add GetRequestedModules() to Module Record"
+ },
  "1519": {
   "ref": "resolve_831",
   "login": "jmdyck",
@@ -131,5 +69,77 @@ var prs = {
   ],
   "base": "69d9e638031c6a16199cbd844b13ccd4b14826b5",
   "title": "Editorial: Remove unused steps from definitions of Contains"
+ },
+ "1493": {
+  "ref": "htmlclosecomment",
+  "login": "leobalter",
+  "revs": [
+   "b6cc8db432c117c9dee96a1a9314e56b669fb51f"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Normative: Extend specs to observe WebReality for HTMLCloseComment"
+ },
+ "1402": {
+  "ref": "template-bugfix",
+  "login": "TimothyGu",
+  "revs": [
+   "5a50eca06776073cdabe58d9478d71dddf3e75e0"
+  ],
+  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
+  "title": "Editorial: Fix SubstitutionTemplate ArgumentListEvaluation"
+ },
+ "1420": {
+  "ref": "parameterize-intrinsics",
+  "login": "bmeck",
+  "revs": [
+   "a2b0457bb1560d1674d624b4233d499df324a7fb"
+  ],
+  "base": "85ce767c86a1a8ed719fe97e978028bff819d1f2",
+  "title": "Allow hosts to reuse an existing Realm Intrinsics record"
+ },
+ "1498": {
+  "ref": "pr-needs-consensus-hostensurecancompilestrings-passthru",
+  "login": "tc39",
+  "revs": [
+   "e344423989fe2599f5ffc86065ac12c28176170e"
+  ],
+  "base": "a09fc232c137800dbf51b6204f37fdede4ba1646",
+  "title": "Provide HostEnsureCanCompileString implementors more context"
+ },
+ "1418": {
+  "ref": "2019-01-explain-SAFE_INTEGER",
+  "login": "gibson042",
+  "revs": [
+   "a6a7b5069e37dec40fac0c2662f81404a57be29e",
+   "4c3b44b9badbd1ad238a16e251662bc632f401aa",
+   "9c0732843f78d257d63e18189cd5ee020d4dfb1e",
+   "41ddc28da1f0be598c1cdf409b6c24582c685b60",
+   "c62904610cee845a7f77051ef6d315406e29aa66",
+   "9c1ca73a518241918c88ca81e875aa124eff00ad",
+   "ed8a3119bb08fb7198cc9feef622e06261b714dc",
+   "9a914a4d650e5bb8452d348c926bc2334104cee4",
+   "75adffca9c7bc41f252e9386c8ad7deefa3040cf"
+  ],
+  "base": "693e09a4b9ce52b060ceda897b042c3f83f0a738",
+  "title": "Editorial: Explain why *_SAFE_INTEGER magnitude is not a power of two"
+ },
+ "1438": {
+  "ref": "object-prop-has",
+  "login": "bmeck",
+  "revs": [
+   "45b775fc0aefce68aaab7f1f242a6373ce2f7584"
+  ],
+  "base": "257cae9f69068704fc9e89a729f54bd7977c9161",
+  "title": "Editorial: Add missing has operation to property operations"
+ },
+ "1522": {
+  "ref": "re_1510",
+  "login": "jmdyck",
+  "revs": [
+   "77e9f1bdc8a9005dde346e8ecc465f4fb8f6d9fb",
+   "759402f8801efc5a60e6119086a9844813556bdd"
+  ],
+  "base": "1595e86b09414b669fcdd23e576ec912685d5f2b",
+  "title": "Editorial: Clarify and expand the discussion of terminal symbols"
  }
 };
