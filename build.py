0ced24e6464268438bd81aaa46a4d1f9173d5bdc
Tooru Fujisawa <arai_a@mac.com>
2020-03-03 04:10:08 +0900

    Test PR comments

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import os
 import sys
+import urllib.request
 
 
 class Logger:
@@ -29,7 +30,46 @@ class FileUtils:
 
 
 class Paths:
+    TOKEN_PATH = os.path.join('.', 'token.json')
     COMMENTS_PATH = os.path.join('.', 'pr-comments.json')
+
+
+class Config:
+    __config_path = os.path.join('.', 'config.json')
+
+    __config = FileUtils.read_json(__config_path)
+
+    API_URL = __config['api_url']
+    PAGE_URL = __config['page_url']
+
+    # Testing now
+    API_URL = 'https://api.github.com/repos/arai-a/workflow-test/'
+
+
+class GitHubAPI:
+    __API_TOKEN = os.environ.get('POST_TOKEN')
+    if not __API_TOKEN and os.path.exists(Paths.TOKEN_PATH):
+        __API_TOKEN = FileUtils.read_json(Paths.TOKEN_PATH)['post_token']
+
+    @classmethod
+    def post(cls, path, query, data):
+        query_string = '&'.join(
+            map(lambda x: '{}={}'.format(x[0], x[1]),
+                query))
+
+        url = '{}{}?{}'.format(Config.API_URL, path, query_string)
+        if cls.__API_TOKEN:
+            headers = {
+                'Authorization': 'token {}'.format(cls.__API_TOKEN),
+            }
+        else:
+            headers = {}
+
+        headers['Content-Type'] = 'application/json'
+
+        req = urllib.request.Request(url, json.dumps(data).encode(), headers)
+        response = urllib.request.urlopen(req)
+        return json.loads(response.read())
 
 
 class StorePRComments:
@@ -63,11 +103,19 @@ class PostPRComments:
         for prnum in comments['new']:
             comments['posted'].append(prnum)
             Logger.info('Posting to PR {}'.format(prnum))
-            # FIXME post here
+
+            # Testing now
+            GitHubAPI.post('issues/2/comments', [], {
+                'body': '{}/?pr={}'.format(Config.PAGE_URL, prnum)
+            })
+
+            # GitHubAPI.post('issues/{}/comments'.format(prnum), [], {
+            #     'body': '{}/?pr={}'.format(Config.PAGE_URL, prnum)
+            # })
 
         comments['new'] = []
 
-        FileUtils.write(Paths.COMMENTS_PATH, json.dumps(comments))
+        #FileUtils.write(Paths.COMMENTS_PATH, json.dumps(comments))
 
 
 class Bootstrap:
